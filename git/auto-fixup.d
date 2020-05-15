554bed6862c8289739154b94d05b2f2090d55ee3
v1ne <v1ne2go@gmail.com>
2020-05-15 18:27:02 +0200

    auto-fixup: diff from HEAD to checkout if !staged
    Before, only unstaged changes would have been taken into account in non-staged
    mode. But the diff should include both staged and non-staged changes.

--- git/auto-fixup.d
+++ git/auto-fixup.d
@@ -213,7 +213,7 @@ auto removedLinesInFiles(const ref Parameters parms, FileInfo fileInfo) {
   }
 
   foreach(line; executeGitCmd(["diff", "-p"]
-      ~ (fileInfo.useStaged ? ["--staged"] : "--" ~ fileInfo.files))) {
+      ~ (fileInfo.useStaged ? ["--staged"] : ["HEAD", "--"] ~ fileInfo.files))) {
     if(line.startsWith("diff --git a/") || line.startsWith("+++ b/") || line.startsWith("index ")) {
       /* ignore */
     } else if(line.startsWith("--- a/")) {
